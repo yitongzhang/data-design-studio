19c4b55b1e36b7c4fb96e200ca9b077e68f7495b
Yitong <zha.yitong@gmail.com>
2018-03-05 19:30:32 -0500

    you can now download svg

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 var draw = SVG('drawing').size(300, 300)
 var rect = draw.rect(100, 100).attr({ fill: '#f06' })
-console.log(draw.svg())
+
+$("#btn-save").click( function() {
+  var content = (draw.svg());
+  var blob = new Blob([content], {type: "text/plain;charset=utf-8"});
+  saveAs(blob, "data-design-export.svg");
+});
